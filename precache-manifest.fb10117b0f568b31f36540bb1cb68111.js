76ce2cb4f8ee0aafc3243d49679dd920ebb22a2f
Santhosh <santhoshmani30@gmail.com>
2020-02-04 19:19:48 +0200

    Updates

---
+++ precache-manifest.fb10117b0f568b31f36540bb1cb68111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4f7fa2969c159672338ba613828227",
+    "url": "/participants/index.html"
+  },
+  {
+    "revision": "c3148be36f30658b7956",
+    "url": "/participants/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5a6f4c6dfefe2e06a34",
+    "url": "/participants/static/js/2.3b92a818.chunk.js"
+  },
+  {
+    "revision": "9c7e6cca0df3be74c612f63119a9d27d",
+    "url": "/participants/static/js/2.3b92a818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3148be36f30658b7956",
+    "url": "/participants/static/js/main.b969c9d1.chunk.js"
+  },
+  {
+    "revision": "a06eedb5012f6f1be72d",
+    "url": "/participants/static/js/runtime-main.8e5f8320.js"
+  },
+  {
+    "revision": "e8a96df58d2b180dc95c6af75e938554",
+    "url": "/participants/static/media/logo.e8a96df5.svg"
+  }
+]);
